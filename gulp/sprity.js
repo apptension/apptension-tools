f06ef42ee88a97dfb3d72ee61a06bc88aa7027a2
Patryk Ziemkowski <patryk.ziem@gmail.com>
2015-08-17 11:54:34 +0200

    Added cache buster to production sprity config.

--- gulp/sprity.js
+++ gulp/sprity.js
@@ -4,6 +4,7 @@ var merge = require('merge-stream');
 var gulpif = require('gulp-if');
 var sprity = require('sprity');
 var env = require('./utils/env');
+var _ = require('lodash');
 
 var config = require('./config')();
 
@@ -11,11 +12,13 @@ var config = require('./config')();
 module.exports = function () {
   var imagesDest = config.paths.tmp;
   var styleDest = config.paths.tmp;
+  var options = _.cloneDeep(config.sprity);
 
   if (env.isProduction()) {
     imagesDest = config.paths.dist;
+    options.cachebuster = true;
   }
 
-  return sprity.src(config.sprity)
+  return sprity.src(options)
     .pipe(gulpif('*.png', gulp.dest(path.join(imagesDest, config.paths.spritesCss)), gulp.dest(styleDest)));
 };
