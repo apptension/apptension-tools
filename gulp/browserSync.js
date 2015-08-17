438b044de8e52765314585c6faa391b18d7bb2e1
Patryk Ziemkowski <patryk.ziem@gmail.com>
2015-08-17 12:52:09 +0200

    Added browser-sync to prodlike config.

--- gulp/browserSync.js
+++ gulp/browserSync.js
@@ -1,10 +1,19 @@
 var browserSync = require('./utils/browserSyncInstance');
 var config = require('./config')();
+var env = require('./utils/env');
 
 
 module.exports = function () {
-  browserSync.init({
-    proxy: config.domain + ':' + config.port,
-    ws: true
-  });
+  if (env.isProduction()) {
+    browserSync.init({
+      server: {
+        baseDir: config.paths.dist
+      }
+    });
+  } else {
+    browserSync.init({
+      proxy: config.domain + ':' + config.port,
+      ws: true
+    });
+  }
 };
