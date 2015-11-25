7c681f6e4492bd0f413c635b8cf1e0bb6a5b305a
Patryk Ziemkowski <patryk.ziem@gmail.com>
2015-11-25 15:07:12 +0100

    Fix serverconfig

--- gulp/config/getServerConfig.js
+++ gulp/config/getServerConfig.js
@@ -1,11 +1,11 @@
 var _ = require('lodash');
 
 const defaultPort = 8000;
-const defaultDomain = 'localhost';
+const defaultDomain = '0.0.0.0';
 
 module.exports = function (userConfig) {
   return {
     port: _.get(userConfig, 'port', defaultPort),
-    domain: _.get(userConfig, defaultDomain, defaultDomain)
+    domain: _.get(userConfig, 'defaultDomain', defaultDomain)
   }
 };
