b3801c42b3b5ac9d8fbe61c6ae75eeb4f4c53f28
Patryk Ziemkowski <patryk.ziem@gmail.com>
2016-02-15 13:14:34 +0100

    Add ifCond helper to hbs

--- gulp/config/getHandlebarsConfig.js
+++ gulp/config/getHandlebarsConfig.js
@@ -7,10 +7,17 @@ module.exports = function (userConfig) {
   var pathsConfig = getPathsConfig(userConfig);
 
 
-
   return _.defaultsDeep(_.get(userConfig, 'handlebars', {}), {
     batch: [
       './' + pathsConfig.dirNames.app + '/' + pathsConfig.dirNames.handlebarsPartials
-    ]
+    ],
+    helpers: {
+      ifCond: function (v1, v2, options) {
+        if (v1 === v2) {
+          return options.fn(this);
+        }
+        return options.inverse(this);
+      }
+    }
   })
 };
