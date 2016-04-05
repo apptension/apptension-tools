7ea22c96697d72504b325b7ddb49e8d638b15ee5
Patryk Ziemkowski <pziemkowski@apptension.com>
2016-04-05 11:41:45 +0200

    Eslint jsx files

--- gulp/eslint.js
+++ gulp/eslint.js
@@ -8,8 +8,10 @@ var config = require('./config');
 module.exports = function () {
   var pathsConfig = config.getPathsConfig();
 
-  return gulp.src(path.join(pathsConfig.paths.eslint, pathsConfig.filePatterns.eslint))
-    .pipe(eslint())
+  return gulp.src([
+    path.join(pathsConfig.paths.src, '**/*.js'),
+    path.join(pathsConfig.paths.src, '**/*.jsx')
+  ]).pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failOnError());
 };
