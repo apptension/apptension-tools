747904d5af8b1d83c4df75f7b8765f3101140602
Andrzej Aleksiejuk <aaleksiejuk@apptension.com>
2016-09-06 13:17:49 +0200

    remove unused import

--- gulp/config/getCssnanoConfig.js
+++ gulp/config/getCssnanoConfig.js
@@ -1,7 +1,5 @@
 var _ = require('lodash');
 
-var getPathsConfig = require('./getPathsConfig');
-
 
 module.exports = function (userConfig) {
   return _.defaultsDeep({}, _.get(userConfig, 'cssnano', {}));
