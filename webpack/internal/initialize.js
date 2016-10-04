14d6fb4c66f5e547896df035be6b9572441fbd33
Patryk Ziemkowski <pziemkowski@apptension.com>
2016-10-04 18:27:40 +0200

    Refactor initialize evolution

--- webpack/internal/initialize.js
+++ webpack/internal/initialize.js
@@ -1,28 +1,26 @@
 import {merge} from 'ramda';
 
-export default (env) => (config) => {
-  return merge({
-    resolve: {
-      extensions: ['', '.js'],
-      alias: {}
-    },
-    plugins: [],
-    module: {
-      loaders: []
-    },
-    node: {
-      __dirname: true,
-      fs: 'empty',
-      net: 'empty',
-      tls: 'empty',
-      dns: 'empty'
-    },
-    browser: {
-      __dirname: true,
-      fs: 'empty',
-      net: 'empty',
-      tls: 'empty',
-      dns: 'empty'
-    }
-  }, config);
-};
+export default (env) => merge({
+  resolve: {
+    extensions: ['', '.js'],
+    alias: {}
+  },
+  plugins: [],
+  module: {
+    loaders: []
+  },
+  node: {
+    __dirname: true,
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    dns: 'empty'
+  },
+  browser: {
+    __dirname: true,
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    dns: 'empty'
+  }
+});
