3645530f566562c0876377533588a19d0a4d47a7
Patryk Ziemkowski <pziemkowski@apptension.com>
2016-10-24 16:05:21 +0200

    Fix addHTMLWebpackPlugin option merge

--- webpack/addHTMLWebpackPlugin.js
+++ webpack/addHTMLWebpackPlugin.js
@@ -5,10 +5,10 @@ import path from 'path';
 import {isProd, isDevOptimized} from '../env';
 
 export default ({htmlExtraOptions = {}} = {}) => ({env, paths}) => evolve({
-  plugins: append(new HtmlWebpackPlugin(merge, {
+  plugins: append(new HtmlWebpackPlugin(merge({
     template: path.join(paths.app, 'index.ejs'),
     inject: 'body',
     environment: env.scriptEnv,
     debug: either(isProd, isDevOptimized)(env)
-  }, htmlExtraOptions))
+  }, htmlExtraOptions)))
 });
