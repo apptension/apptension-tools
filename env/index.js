213a42098132b71020ec24cd57d71794da169b93
Patryk Ziemkowski <pziemkowski@apptension.com>
2016-10-24 14:33:39 +0200

    Refactor env/index.js file

--- env/index.js
+++ env/index.js
@@ -1,35 +1,3 @@
-import {propEq} from 'ramda';
+export * from './definitions';
 
-const ENV_DEV = 'development';
-const ENV_DEV_OPTIMIZED = 'development-optimized';
-const ENV_PROD = 'production';
-
-const buildEnv = envName => {
-  /**
-   * @param options
-   */
-  return options => Object.freeze({
-    devServer: {
-      domain: 'localhost',
-      port: 8000
-    },
-    scriptEnv: 'development',
-    ...options,
-    envName
-  });
-};
-
-const envNameEq = propEq('envName');
-
-
-export const dev = buildEnv(ENV_DEV);
-
-export const devOptimized = buildEnv(ENV_DEV_OPTIMIZED);
-
-export const production = buildEnv(ENV_PROD);
-
-export const isProd = envNameEq(ENV_PROD);
-
-export const isDev = envNameEq(ENV_DEV);
-
-export const isDevOptimized = envNameEq(ENV_DEV_OPTIMIZED);
+export * from './checks';
--- test/env/env.spec.js
+++ test/env/env.spec.js
@@ -1,17 +1,17 @@
 import assert from 'power-assert';
 
-import {dev, devOptimized, production} from '../../env/index';
+import {dev, devOptimized, production, ENV_DEV, ENV_DEV_OPTIMIZED, ENV_PROD} from '../../env/index';
 
 describe('env', () => {
   const testEnv = (env, envName) => {
     describe(envName, () => {
       it(`should set envName property to ${envName}`, () => {
-        assert.equal(env({}).envName, envName)
+        assert.equal(env({}).envName, envName);
       });
     });
   };
 
-  testEnv(dev, 'development');
-  testEnv(devOptimized, 'development-optimized');
-  testEnv(production, 'production');
+  testEnv(dev, ENV_DEV);
+  testEnv(devOptimized, ENV_DEV_OPTIMIZED);
+  testEnv(production, ENV_PROD);
 });
